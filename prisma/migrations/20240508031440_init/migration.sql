92b50313fd73d752e3f3e09d4664c2f32a4fa7fe
makaragessskylie <makarageskyliee@gmail.com>
2024-05-08 14:48:59 +0800

    update migrations

--- prisma/migrations/20240508031440_init/migration.sql
+++ prisma/migrations/20240508031440_init/migration.sql
@@ -11,12 +11,24 @@ CREATE TABLE "admin" (
 );
 
 -- CreateTable
+CREATE TABLE "wisatawan" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "nama" TEXT NOT NULL,
+    "alamat" TEXT NOT NULL,
+    "no_hp" TEXT NOT NULL,
+
+    CONSTRAINT "wisatawan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "objek_wisata" (
     "id" TEXT NOT NULL,
     "nama" TEXT NOT NULL,
     "alamat" TEXT NOT NULL,
     "keterangan" TEXT NOT NULL,
-    "rute" TEXT NOT NULL,
+    "waktu_operasional" TEXT NOT NULL,
 
     CONSTRAINT "objek_wisata_pkey" PRIMARY KEY ("id")
 );
@@ -30,5 +42,11 @@ CREATE TABLE "gambar" (
     CONSTRAINT "gambar_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "admin_email_key" ON "admin"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "wisatawan_email_key" ON "wisatawan"("email");
+
 -- AddForeignKey
-ALTER TABLE "gambar" ADD CONSTRAINT "gambar_objek_wisata_id_fkey" FOREIGN KEY ("objek_wisata_id") REFERENCES "objek_wisata"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "gambar" ADD CONSTRAINT "gambar_objek_wisata_id_fkey" FOREIGN KEY ("objek_wisata_id") REFERENCES "objek_wisata"("id") ON DELETE CASCADE ON UPDATE CASCADE;
